a8e5f89a51e84bc573bf8a70e469341de2afc831
craigambrose <craig@craigambrose.com>
2016-03-11 11:33:21 +1300

    bootstrap header

--- client/components/App.jsx
+++
@@ -1,8 +0,0 @@
-// App component - represents the whole app
-App = React.createClass({
-  render() {
-    return (
-      <div className="container">hello world</div>
-    );
-  }
-});
--- explicit-consent.jsx
+++ explicit-consent.jsx
@@ -1,5 +1,11 @@
 if (Meteor.isClient) {
-  Meteor.startup(function () {
-    React.render(<App />, document.getElementById("render-target"));
+  System.import('/client/components/App').then(function(module){
+      // default export is attached as default property
+      // all named exports are attached by their name
+      var App = module.default;
+
+      Meteor.startup(function () {
+        ReactDOM.render(<App />, document.getElementById("render-target"));
+      });
   });
 }
